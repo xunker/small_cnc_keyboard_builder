7a1ae25008a7d798c88b1a5f6dae558d90d27d1b
Matthew Nielsen <xunker@pyxidis.org>
2019-12-10 17:02:00 -0700

    Way too much PITA to get filesystem mocking working, jeez

--- test/lib/config.test.js
+++ test/lib/config.test.js
@@ -1,6 +1,13 @@
 'use strict'
 
+jest.mock('fs')
 const subject = require('../../lib/config')
+
+beforeEach(() => {
+  const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {
+    console.log("process.exit() called, but ignored")
+  })
+})
 
 describe('config objects', () => {
   ["info", "global", "sections", "layers", "operations", "dimensions"].forEach((objName) => {
@@ -25,23 +32,42 @@ describe('config objects', () => {
 // })
 
 describe('.stabilizerSpacingForWidth', () => {
+  beforeEach(() => {
+    jest.resetModules()
+    jest.resetAllMocks()
+  })
   describe('dimensions -> stabilizers -> cutouts -> spacing is a single number', () => {
     beforeEach(() => {
-      subject.loadDefaults()
-
-      subject.dimensions.stabilizers.cutouts.spacing = 25
-      console.log(subject.global.dimensions)
+      let config = {
+        "global": {
+          "dimensions": {
+            "stabilizers": {
+              "cutouts": {
+                "spacing": 25
+              }
+            }
+          }
+        }
+      }
+      require('fs').__setMockFiles({
+        'configFilename.js': JSON.stringify(config)
+      })
     })
 
     it('returns that number regardless of `width`', () => {
+      const subject = require('../../lib/config')
+
+      subject.loadConfiguration({ configFile: 'configFilename.js' })
+
       expect(subject.stabilizerSpacingForWidth(2)).toBe(25)
       expect(subject.stabilizerSpacingForWidth(6)).toBe(25)
+
     })
   })
 
   describe('dimensions -> stabilizers -> cutouts -> spacing is a an object', () => {
     beforeEach(() => {
-      subject.loadDefaults()
+      subject.loadConfiguration({})
 
       subject.dimensions.stabilizers.cutouts.spacing = {
         6: 76.2,
@@ -69,12 +95,6 @@ describe('.stabilizerSpacingForWidth', () => {
     it('returns 76.2 for width 7', () => {
       expect(subject.stabilizerSpacingForWidth(7)).toBe(76.2)
     })
-
-    // [[2, 24], [3, 24], [6, 76.2], [6.25, 76.2], [7, 76.2]].forEach((pair) => {
-      // it(`returns ${pair[1]} for width of ${pair[0]}`, () => {
-      //   expect(subject.stabilizerSpacingForWidth(pair[0])).toBe(pair[1])
-      // })
-    // })
   })
 
 
